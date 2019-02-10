8a6561c3fe5a59a5276a4a68f33450877ec39bb0
Minifyre <minifyre@gmail.com>
2019-02-10 09:51:56 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import silo from './node_modules/silo/index.js'
+import truth from './node_modules/truth/truth.mjs'
+import v from './node_modules/v/v.mjs'
+
+const {config,util,logic,output,input}=silo
+//@todo find & replace "custom" & "element" with proper values
+export default silo(async function(initialState)
+{
+	customElements.define('v-pane',silo.element)
+
+
+	//@todo these should be in silo.customElement constructor... 
+	const
+	state=logic(initialState),
+	render=truth.compile(({state})=>v.render(document.body,state,output))
+
+	truth(state,render)
+})
+silo.element=class extends silo.customElement
+{
+	constructor(state={})
+	{
+		super(state,silo)
+	}
+}
