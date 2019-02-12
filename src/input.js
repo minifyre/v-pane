26a3767f085066d67cb21e90f2a75151141d2f12
Minifyre <minifyre@gmail.com>
2019-02-11 19:25:26 -0600

    +right click handler
    Added right click handler to each pane and a child to make sure the event handler does not interfere with right clicks inside the child content.

---
+++ src/input.js
@@ -0,0 +1,8 @@
+input.rightClick=function(state,evt)
+{
+	if(!evt.target.matches('.pane')) return
+
+	evt.preventDefault()
+
+	
+}
--- src/output.js
+++ src/output.js
@@ -1,11 +1,25 @@
 output.render=function({state})
 {
-	return Object.values(state.file.panes)
+	const contextmenu=curry(input.rightClick,state)
+
+	return [v('style',{},`
+	.pane
+	{
+		box-sizing:border-box;
+		padding:0.5rem;
+	}
+	.pane div
+	{
+		background-color:#fc0;
+		height:100%;
+		width:100%;
+	}`)]
+	.concat(Object.values(state.file.panes)
 	.filter(x=>!!x)
 	.map(function(pane)
 	{
 		const style=util.box2style(pane)
 
-		return v('.pane',{style})
-	})
+		return v('.pane',{style,on:{contextmenu}},v('div'))
+	}))
 }
