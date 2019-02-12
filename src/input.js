091923b531d0475d76f92ab7dc5b8decad991720
Minifyre <minifyre@gmail.com>
2019-02-11 21:05:21 -0600

    +input.renderPopup()
    This draws the popup at the correct place (x,y,z) based on the user input.

--- src/input.js
+++ src/input.js
@@ -11,3 +11,9 @@ input.rightClick=function(sandbox,evt)
 	popup.x=(evt.pageX-box.x)/box.width*100
 	popup.y=(evt.pageY-box.y)/box.height*100
 }
+input.renderPopup=function(sandbox,evt)
+{
+	const {x,y}=sandbox.state.view.popup
+
+	evt.target.setAttribute('style',`left:${x}%; top:${y}%;`)
+}
