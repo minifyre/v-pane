bfd198a780d27f63bed5105538258126ad8deece
Minifyre <minifyre@gmail.com>
2019-02-15 14:35:56 -0600

    bug fix
    Ensured that pane had the minimum properties by using assign instead of requiring the user to call util.mk().

--- src/logic.js
+++ src/logic.js
@@ -6,7 +6,9 @@ logic.normalize=function(state)
 
 	return state
 }
-logic.addPane=function(state,pane=util.mk({x:0,y:0,width:100,height:100}))
+logic.addPane=function(state,paneOpts={})
 {
+	const pane=util.mk({x:0,y:0,width:100,height:100},paneOpts)
+
 	state.file.data.panes[pane.id]=pane
 }
