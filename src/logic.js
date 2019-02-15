6f68a3a6bd8b0489ca006768a43eeebcc5d14e3c
Minifyre <minifyre@gmail.com>
2019-02-15 14:26:46 -0600

    +logic.addPane

--- src/logic.js
+++ src/logic.js
@@ -1,10 +1,12 @@
 logic.normalize=function(state)
 {
 	if(Object.keys(state.file.data.panes).length) return state
-	//state must always have at least one pane
-	const pane=util.mk({x:0,y:0,width:100,height:100})
 
-	state.file.data.panes[pane.id]=pane
+	logic.addPane(state)
 
 	return state
+}
+logic.addPane=function(state,pane=util.mk({x:0,y:0,width:100,height:100}))
+{
+	state.file.data.panes[pane.id]=pane
 }
